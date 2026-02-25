e29ce3bf2b01174f18b27f0f525a0d79c98cce80
Adriele Carolina Rodrigues Rocha <1046483@senacminas.edu.br>
2026-02-25 15:47:58 -0300

    teste da tela

--- python/app.py
+++ python/app.py
@@ -5,80 +5,90 @@ from modulos.aluno import Aluno
 import sys
 
 from PySide6.QtWidgets import(
-
     QApplication,
     QWidget,
     QVBoxLayout,
     QLabel,
     QLineEdit,
-    QPushButton
+    QPushButton,
+    QMessageBox
 )
-
-def cadastro():
-    aluno = Aluno(
-        campo_nome.text(),
-        campo_email.text(),
-        campo_cpf.text(),
-        campo_telefone.text(),
-        campo_endereco.text(),
-    )
+class TelaCadastro():
+    def __init__(self):
+        self.app = QApplication(sys.argv)
+        self.janela = QWidget()
+        self.layout = QVBoxLayout()
+        self.banco = MySQL()
+        
+        self.campos = {}
+        self.configurar_janela ()
+        self.criar_componentes()
     
-    banco = MySQL()
-    banco.connect()
+    def configurar_janela(self): 
+        
+        self.janela.setWindowTitle("Cadastrar Aluno")
+        # Adaptar redimensionamento para tamanho dinamico
+        self.janela.resize(1200, 600)
+        self.janela.setLayout(self.layout)
+        
+    def criar_componentes(self):
+        componentes = {
+            "nome":"Digitar seu nome:",
+            "email": "Digitar seu email:",
+            "cpf":"Digitar seu cpf:",
+            "telefone":"Digitar seu telefone:",
+            "endereco":"Digitar seu endereco:"
+               
+        }
+        for chave, texto  in componentes.items():
+            label = QLabel(texto)
+            campo = QLineEdit()
+            
+            self.layout.addWidget(label)
+            self.layout.addWidget(campo)
+            
+            self.campos[chave] = campo
+            
+        botao_cadastro = QPushButton("Cadatrar")
+        self.layout.addWidget(botao_cadastro)
+        
+        botao_cadastro.clicked.connect(self.cadastrar)
+        
+    def cadastrar(self):
+        aluno = Aluno(
+            self.campos["nome"].text(),
+            self.campos["email"].text(),
+            self.campos["cpf"].text(),
+            self.campos["telefone"].text(),
+            self.campos["endereco"].text(),
+        )
+        try:
+            self.banco.connect()
+            aluno.cadastrar(self.banco)
+            QMessageBox.information(
+                self.janela,
+                "sucesso",
+                "Aluno Cadastrado!"
+            )
+            self.limpar_campos()
+            
+        except Exception as e:
+            QMessageBox.critical(
+                self.janela,
+                "Erro",
+                f"Erro ao castrar: {e}"
+            )
+            
+        finally:
+            self.banco.disconnect()
     
-    aluno.cadastrar(banco)
-    banco.disconnect()
-
-app = QApplication(sys.argv)
-
-janela = QWidget()
-janela.setWindowTitle("Primeiro Janela")
-janela.resize(1200, 600)
+    def limpar_campos(self):
+        for campo in self.campos.values():
+            campo.clear()
 
 
-layout = QVBoxLayout()
-
-#Componentes
-lebal_nome= QLabel("Digite seu nome:")
-campo_nome=QLineEdit()
-
-lebal_email= QLabel("Digite seu email:")
-campo_email= QLineEdit()
-
-lebal_cpf= QLabel("Digite seu cpf:")
-campo_cpf= QLineEdit()
-
-lebal_telefone= QLabel("Digite seu telefone:")
-campo_telefone= QLineEdit()
-
-lebal_endereco= QLabel("Digite seu endereço:")
-campo_endereco= QLineEdit()
-
-botao= QPushButton("Cadastrar")
-
-# Adicionar componentes à janela 
-layout.addWidget(lebal_nome)
-layout.addWidget(campo_nome)
-
-layout.addWidget(lebal_cpf)
-layout.addWidget(campo_cpf)
-
-layout.addWidget(lebal_email)
-layout.addWidget(campo_email)
-
-layout.addWidget(lebal_telefone)
-layout.addWidget(campo_telefone)
-
-layout.addWidget(lebal_endereco)
-layout.addWidget(campo_endereco)
-
-
-layout.addWidget(botao)
-
-janela.setLayout(layout)
-
-botao.clicked.connect(cadastro)
-
-
-janela.show()
-sys.exit(app.exec())
+if __name__=="`__main__":
+    tela = TelaCadastro()
+    tela.janela.show()
+    
+    sys.exit(tela.app.exec())
